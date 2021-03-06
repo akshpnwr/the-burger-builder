aefd3a8bd9fdb8934c2692780f548f5aa735239d
akshpnwr <akashpanwar9976@gmail.com>
2021-03-06 13:22:50 +0530

    added navigation for desktop

--- src/components/Navigation/Sidedrawer/SideDrawer.js
+++ src/components/Navigation/Sidedrawer/SideDrawer.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/Navigationitems';
+import classes from './SideDrawer.css';
 
-const toolbar = (props) => {
+const sideDrawer = (props) => {
   return (
-    <header className={classes.Toolbar}>
-      <div>MENU</div>
+    <div className={classes.SideDrawer}>
       <div className={classes.Logo}>
         <Logo />
       </div>
-      <NavigationItems />
-    </header>
+      <nav>
+        <NavigationItems />
+      </nav>
+    </div>
   );
 };
 
-export default toolbar;
+export default sideDrawer;
