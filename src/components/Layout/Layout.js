12799f624e018d1289c1d35c0c63ae09d4f0678f
akshpnwr <akashpanwar9976@gmail.com>
2021-03-06 10:57:04 +0530

    toolbar and logo added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
   <Aux>
-    <div>Toolbar, SideDrawer and Backdrop</div>
-    <main className={classes.content}>{props.children}</main>
+    <Toolbar />
+    <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
 
