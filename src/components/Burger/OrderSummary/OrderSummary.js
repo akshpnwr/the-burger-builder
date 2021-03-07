85e7ddcac82f0c9964141c6773ffd58276764eec
akshpnwr <akashpanwar9976@gmail.com>
2021-03-07 13:09:48 +0530

    Improving performance and folder structure

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,38 @@
-import React from 'react';
-import Aux from '../../../hoc/Auxiliary';
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredients = Object.keys(props.ingredients).map((key) => {
+class OrderSummary extends Component {
+  componentDidUpdate() {
+    console.log('[OrderSummary] willUpdate');
+  }
+
+  render() {
+    const ingredients = Object.keys(this.props.ingredients).map((key) => {
+      return (
+        <li key={key}>
+          <span style={{ textTransform: 'capitalize' }}>{key}</span> :{' '}
+          {this.props.ingredients[key]}
+        </li>
+      );
+    });
+
     return (
-      <li key={key}>
-        <span style={{ textTransform: 'capitalize' }}>{key}</span> :{' '}
-        {props.ingredients[key]}
-      </li>
+      <Aux>
+        <h3>Your order : </h3>
+        <p>A delicious burger with the following ingredients : </p>
+        <ul>{ingredients}</ul>
+        <h3>Price : {this.props.price}</h3>
+        <p>Continue to checkout : </p>
+        <Button type={'Danger'} clicked={this.props.cancel}>
+          Cancel
+        </Button>
+        <Button type={'Success'} clicked={this.props.continue}>
+          Continue
+        </Button>
+      </Aux>
     );
-  });
+  }
+}
 
-  return (
-    <Aux>
-      <h3>Your order : </h3>
-      <p>A delicious burger with the following ingredients : </p>
-      <ul>{ingredients}</ul>
-      <h3>Price : {props.price}</h3>
-      <p>Continue to checkout : </p>
-      <Button type={'Danger'} clicked={props.cancel}>
-        Cancel
-      </Button>
-      <Button type={'Success'} clicked={props.continue}>
-        Continue
-      </Button>
-    </Aux>
-  );
-};
-
-export default orderSummary;
+export default OrderSummary;
