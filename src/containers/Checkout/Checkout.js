ddcae9be3ad7ecc49261b293a37ee47916492afc
akshpnwr <akashpanwar9976@gmail.com>
2021-03-19 21:25:06 +0530

    Building the contact component

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import { Route } from 'react-router-dom';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
   state = {
@@ -50,6 +52,10 @@ class Checkout extends Component {
           ingredients={this.state.ingredients}
           checkoutCancel={this.checkoutCancelHandler}
           checkoutContinue={this.checkoutContinueHandler}
+        />
+        <Route
+          path={`${this.props.match.path}/contact-data`}
+          component={ContactData}
         />
       </div>
     );
