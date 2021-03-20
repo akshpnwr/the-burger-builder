4f2d9e5077f01930a953119f02fca53d16d289b1
akshpnwr <akashpanwar9976@gmail.com>
2021-03-20 20:11:57 +0530

    Implementing nav links

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Orders from './containers/Checkout/Orders/Orders';
 
 class App extends Component {
@@ -10,13 +10,11 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <BrowserRouter>
-            <Switch>
-              <Route exact path="/" component={BurgerBuilder} />
-              <Route path="/checkout" component={Checkout} />
-              <Route path="/orders" component={Orders} />
-            </Switch>
-          </BrowserRouter>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
+            <Route exact path="/" component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
